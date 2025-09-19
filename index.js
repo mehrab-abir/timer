1a81977e74e1efcec8448022f4af8e76d80e28d7
Mehrab-Abir <mjabir@myseneca.ca>
2025-09-18 23:25:16 -0400

    Files Added

---
+++ index.js
@@ -0,0 +1,35 @@
+const start = document.querySelector(".start");
+const end = document.querySelector(".end");
+const showTimer = document.querySelector(".showTimer");
+
+let intervalID;
+
+start.addEventListener("click",function(){
+    const input = document.getElementById("input");
+    let inputValue = Number(input.value);
+    showTimer.innerText = inputValue;
+    input.value = '';
+
+    alert(`${inputValue} seconds timer starting now, will end after ${inputValue} seconds if not ended before that.`);
+
+    intervalID = setInterval(function(){
+        inputValue--;
+
+        if(inputValue <= 5){
+            showTimer.style.color = "#ff5757"; 
+        }
+
+        if(inputValue === 0){
+            clearInterval(intervalID);
+            showTimer.style.color = "#ffffff"; 
+        }
+
+        showTimer.innerText = inputValue;
+    },1000)
+
+    // console.log(inputValue)
+})
+
+end.addEventListener('click',function(){
+    clearInterval(intervalID)
+})
